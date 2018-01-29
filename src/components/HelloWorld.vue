75b56806721cf834a35a3c103e7b8ac34478be7b
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-01-28 14:28:43 -1000

    prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,54 +12,59 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
       socket: null,
       userInputTxt: null,
-      msg: '',
-      roomTitle: '',
-      roomDesc: '',
-      exits:'',
-    }
+      msg: "",
+      loginScreen: "",
+      roomTitle: "",
+      roomDesc: "",
+      exits: ""
+    };
   },
   created() {
-    const socket = new WebSocket(wsurl)
+    const socket = new WebSocket(wsurl);
 
-    socket.addEventListener('open', () => {
-        console.log('WS Connection Opened!')
-    })
+    socket.addEventListener("open", () => {
+      console.log("WS Connection Opened!");
+    });
 
-    socket.addEventListener('close', () => {
-        console.log('WS Closed')
-    })
-    
-    socket.addEventListener('message', e => {
-      const data = JSON.parse(e.data)[1]
-      console.log('ws data from evennia', data);
+    socket.addEventListener("close", () => {
+      console.log("WS Closed");
+    });
+
+    socket.addEventListener("message", e => {
+      const data = JSON.parse(e.data)[1];
+      console.log("ws data from evennia", data);
       this.msg = text[0];
-    })
+    });
     this.$root.socket = socket;
-    
   },
   methods: {
-    sendTextToEvennia: function(e) {  
-      console.log('sendTextToEvennia: ', e)
-      const lines = this.userInputTxt.trim().replace(/[\r]+/,"\n").replace(/[\n]+/, "\n").split("\n");
-      console.log('lines', lines)
-      const data = ["text", lines, {}]
+    sendTextToEvennia: function(e) {
+      console.log("sendTextToEvennia: ", e);
+      const lines = this.userInputTxt
+        .trim()
+        .replace(/[\r]+/, "\n")
+        .replace(/[\n]+/, "\n")
+        .split("\n");
+      console.log("lines", lines);
+      const data = ["text", lines, {}];
       if (this.userInputTxt) {
-        this.$root.socket.send(JSON.stringify(data))
+        this.$root.socket.send(JSON.stringify(data));
       }
       this.userInputTxt = null;
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
