89eaf88dd836900da63d3e55707d85e61959f86d
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-02-28 10:20:47 -1000

    add sentry.io error tracking

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
 import App from "./App";
 import router from "./router"
 import Evennia from "./utils/evennia";
+
+// Activate sentry.io error tracking
+// TODO: Refactor sentry access ID to env variable when opening repo to public
+Raven
+    .config('https://fa7767deeb7049ec95c890522cdafaeb@sentry.io/296217')
+    .addPlugin(RavenVue, Vue)
+    .install();
 
 
 // Evennia.js needs these two global variables to initialize
